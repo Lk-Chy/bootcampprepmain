3024a77a93e75108a36f7892af913ed799129658
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 16:40:10 +0545

    completed  truth teller

--- bootcamp-prep-main/pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
+++ bootcamp-prep-main/pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
@@ -4,5 +4,21 @@ let boolean1 = true, boolean2 = true;
 // let boolean1 = false, boolean2 = false;
 
 let result;
+// ### Truth Teller
+
+// Given two boolean values boolean1 and boolean2, set the result variable to:
+//   - 'both' if both boolean1 and boolean2 are true
+//   - 'one' if only one of boolean1 and boolean2 are true
+//   - 'none' if neither value is true
+
 
 // YOUR CODE BELOW
+if (boolean1 === true && boolean2 === true){
+    result = 'both';
+}
+else if( boolean1 === true || boolean2 === true){
+    result = 'one';
+}
+else{
+    result = None;
+}
