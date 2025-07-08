e379341b0f9da70c13d33b4442415dcdf51b24dd
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 18:17:13 +0545

    completed max of three

--- bootcamp-prep-main/pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+// ### Max of Three
+
+// Write a function `maxOfThree` that accepts three number arguments. `maxOfThree`
+// should return largest number of the three.
+
+// ```javascript
+maxOfThree(5, 4, 10); // => 10
+
+maxOfThree(7, 7, 4); // => 7
+// ```
+
+
+function maxOfThree(a,b,c){
+    if (a>b && a>c){
+        return a;
+    }
+    else if (b>a && b>c){
+        return b;
+    }
+    else{
+        return c;
+    }
+}
