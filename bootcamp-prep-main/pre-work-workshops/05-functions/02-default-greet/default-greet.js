152fe465fb26fbc2ee263300da5fb43a3116106c
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 17:49:14 +0545

    completed deafult greet

--- bootcamp-prep-main/pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ bootcamp-prep-main/pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+// ### Default Greet
+
+// Define a function, `defaultGreet`, that takes a first and a last name.
+// `defaultGreet` should return a string greeting for the given names. If the user
+// does not define a last name, assume the last name is 'Doe'.
+
+// ```javascript
+defaultGreet("Chaka", "Khan"); // => 'Hi Chaka Khan!'
+defaultGreet("John"); // => 'Hi John Doe!'
+// ```
+
+function defaultGreet(a,b='Doe') {
+    // if( b === undefined){
+    //     b='Doe';
+    // }
+    return ('Hi '+ a+' '+b+'!');
+}
+
+
+// const defaultGreet = (a,b='Doe') => {
+//     return ('Hi '+ a+' '+b+'!');
+// };
