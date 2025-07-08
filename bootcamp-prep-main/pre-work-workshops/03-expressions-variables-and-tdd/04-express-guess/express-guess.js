713ee9264b7da272ada9a5969a85f18d83c01652
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 12:17:22 +0545

    completed express-guess

--- bootcamp-prep-main/pre-work-workshops/03-expressions-variables-and-tdd/04-express-guess/express-guess.js
+++ bootcamp-prep-main/pre-work-workshops/03-expressions-variables-and-tdd/04-express-guess/express-guess.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+
+// ### Express Guess
+
+//  Look at the expression below. Guess what the result of the expression will be,
+//  and store your guess in a variable called myGuess.
+
+//  Expression: 20 * (50 / (5 * 2) + 15)
+
+let myGuess = 400;
