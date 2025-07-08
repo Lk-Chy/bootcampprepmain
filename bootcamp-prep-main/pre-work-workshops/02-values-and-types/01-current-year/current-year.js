9f22f1261bd66f831cf031c468581a60c6693ba9
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 12:00:20 +0545

    completed current-year

--- bootcamp-prep-main/pre-work-workshops/02-values-and-types/01-current-year/current-year.js
+++ bootcamp-prep-main/pre-work-workshops/02-values-and-types/01-current-year/current-year.js
@@ -1,1 +1,7 @@
 // YOUR CODE BELOW
+// Create an Arithmetic Expression that uses three mathematical operators (+,-,/,*) that returns the date
+// of the current year.
+
+// EX: 2000 + 18 - 2 * 1;
+
+2000 + 30 - 5 * 2;
