db51ab53466d5e7e70c8b80e330e823b8d0884b7
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 12:15:04 +0545

    completed my first sum

--- bootcamp-prep-main/pre-work-workshops/03-expressions-variables-and-tdd/03-first-sum/first-sum.js
+++ bootcamp-prep-main/pre-work-workshops/03-expressions-variables-and-tdd/03-first-sum/first-sum.js
@@ -3,3 +3,11 @@ let num2 = 385;
 let num3 = 779;
 
 // YOUR CODE BELOW
+
+// ### First Sum
+
+// Given three numbers, assign the sum of all three numbers to a variable called
+// myFirstSum.
+
+myFirstSum = num1 + num2 + num3;
+
