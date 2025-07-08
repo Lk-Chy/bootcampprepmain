6b6d00197a5273e0c5b324a71524f65a8ef9e860
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 12:05:34 +0545

    completed lowercase

--- bootcamp-prep-main/pre-work-workshops/02-values-and-types/04-to-lowercase/to-lowercase.js
+++ bootcamp-prep-main/pre-work-workshops/02-values-and-types/04-to-lowercase/to-lowercase.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+// ### To Lowercase
+
+// Create a String by surrounding characters with an apostrophe or quotation marks.  Call the toLowerCase() method
+// on the string.
+
+// EX: "FULLSTACK ACADEMY".toLowerCase()
+// OUTPUT: "fullstack academy";
+
+'LOKESH CHAUDAHRY'.toLowerCase();
