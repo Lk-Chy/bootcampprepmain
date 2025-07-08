c14b65b2d7d34c4a9bf099490f5595006f42c50a
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 18:17:04 +0545

    completed last character

--- bootcamp-prep-main/pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+// ### Last Character
+
+// Define a function `lastCharacter` that accepts two strings as arguments.
+// `lastCharacter` should return true if both strings end with the same character.
+// Otherwise, `lastCharacter` should return false.
+
+
+// ```javascript
+lastCharacter('apples', 'pumpkins'); // => true
+
+lastCharacter('marker', 'pen'); // => false
+// ```
+
+function lastCharacter(a,b){
+    if (a[a.length -1] === b[b.length -1]){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
