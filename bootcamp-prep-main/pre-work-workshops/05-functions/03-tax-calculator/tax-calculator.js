cf363d78eaa972037f65e8a4006a770179c0866f
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 17:49:23 +0545

    completed tax calculator

--- bootcamp-prep-main/pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ bootcamp-prep-main/pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+// ### Tax Caclulator
+
+// Define a function `taxCalculator` that accepts the price of an item and a state.
+// `taxCalculator` should return the after-tax cost of the item, assuming NY's tax
+// is 4% and NJ's sales tax is 6.625%.
+
+// ```javascript
+taxCalculator(100, 'NY'); // => 104
+taxCalculator(100, 'NJ'); // => 106.625
+// ```
+
+function taxCalculator(price,state){
+    if(state === 'NY'){
+        return (1.04*price);
+    }
+    else if(state === 'NJ'){
+        return (1.06625*price);
+    }
+};
