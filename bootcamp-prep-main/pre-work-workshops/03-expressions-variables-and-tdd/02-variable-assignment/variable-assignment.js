1d503995327ad0fcc68a2a80aac5ba7d810b5d7e
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 12:13:29 +0545

    variable assignment completed

--- bootcamp-prep-main/pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
+++ bootcamp-prep-main/pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+// ### Variable Assignment
+
+// Create a variable called myFirstName. Assign myFirstName a string value that
+// represents your first name.
+
+// Create another variable called myFavoriteNum. Assign myFavoriteNum a number
+// value that represents your favorite number!
+
+// Create a third variable, havingFun. Assign havingFun a boolean value that
+// reflects whether or not you're having fun!
+
+let myFirstName = 'Lokesh';
+let myFavoriteNum=9;
+let havingFun = true ;
