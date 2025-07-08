7826b7ae8ffae4c1eb0430f9f54a25c58dcc4c06
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 16:37:03 +0545

    completed founder-finder

--- bootcamp-prep-main/pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ bootcamp-prep-main/pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -4,4 +4,19 @@ let name = 'David';
 
 let found;
 
+
+
+// ### Founder Finder
+
+// Given a string value name, set the found variable to:
+//   - true if the name is equal to 'David'
+//   - true if the name is equal to 'Nimit'
+//   - false if the name is equal to 'Someone else'
+
 // YOUR CODE BELOW
+if(name === 'David' || name === 'Nimit'){
+    found = true;
+}
+else {
+    found = false;
+}
