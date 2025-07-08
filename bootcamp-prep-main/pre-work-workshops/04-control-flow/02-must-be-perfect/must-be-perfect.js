9f2d24e375b107b56a11b06a1ffc2d93f333c2af
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 16:37:17 +0545

    completed must be perfect

--- bootcamp-prep-main/pre-work-workshops/04-control-flow/02-must-be-perfect/must-be-perfect.js
+++ bootcamp-prep-main/pre-work-workshops/04-control-flow/02-must-be-perfect/must-be-perfect.js
@@ -6,4 +6,23 @@ let word = 'carrots';
 
 let lastWord;
 
+// ### Must Be Perfect
+
+// Given a string value word, set the lastWord variable to:
+//   - the upper-cased string stored in word if the word starts with the letter p
+//     and has a length of 7
+//   - the unmodified string stored in word
+
+// For example, if the word is equal to 'apples', lastWord should be equal to
+// 'apples'. If the word is equal to 'perfect', 'lastWord' should be equal to
+// 'PERFECT'.
+
 // YOUR CODE BELOW
+
+if (word[0] === 'p' && word.length===7){
+    lastWord = word.toUpperCase();
+}
+else{
+        lastWord = word;
+}
+
