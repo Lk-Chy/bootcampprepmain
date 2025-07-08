06c212668593af5b371ffc3fbda16d1abf3cbe45
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 18:16:55 +0545

    completed do you play

--- bootcamp-prep-main/pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+// ### Do You Play the Theremin
+
+// Define a function `doYouPlayTheTheremin` that accepts a string as an argument.
+// `doYouPlayTheTheremin` should return true if the inputted string starts with the
+// letters `s` or `S`. Otherwise, `doYouPlayTheTheremin` should return false.
+
+// ```javascript
+doYouPlayTheTheremin('Griffin'); // => false
+doYouPlayTheTheremin('Scott'); // => true
+// ```
+
+function doYouPlayTheTheremin(a){
+if (a[0]==='s' || a[0]==='S'){
+    return true;
+}
+else{
+    return false;
+}
+}
