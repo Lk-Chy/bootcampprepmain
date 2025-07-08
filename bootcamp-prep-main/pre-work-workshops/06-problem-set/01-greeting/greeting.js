0b273b8f897bcae7c0a461296a616976f3ad8790
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 18:05:47 +0545

    completed greeting

--- bootcamp-prep-main/pre-work-workshops/06-problem-set/01-greeting/greeting.js
+++ bootcamp-prep-main/pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+// ### Greeting
+
+// Define a function `greeting` that accepts an optional string argument called
+// name. `greeting` should return a personalized string if the name is present.
+
+// ```javascript
+greeting('Sadie'); // => Hello Sadie!
+greeting(); // => Hello!
+// ```
+
+function greeting(name){
+if (name=== undefined){
+    return "Hello!";
+}
+else{
+    return ("Hello "+name+'!');
+}
+}
+
+// }
+// {
+//     if (name===""){return "Hello!";}
+//     return ("Hello "+name+'!');
+// }
