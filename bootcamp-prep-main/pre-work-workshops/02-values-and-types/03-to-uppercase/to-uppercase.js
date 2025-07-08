75de56066b80a42360bd257a12d6dd36787c5f34
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-08 12:05:23 +0545

    completed uppercase

--- bootcamp-prep-main/pre-work-workshops/02-values-and-types/03-to-uppercase/to-uppercase.js
+++ bootcamp-prep-main/pre-work-workshops/02-values-and-types/03-to-uppercase/to-uppercase.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+
+
+// Create a String by surrounding characters with an apostrophe or quotation marks.
+// Call the toUpperCase() method on the string.
+
+// EX: "apples".toUpperCase()
+// OUTPUT: "APPLES"
+
+"lokesh".toUpperCase();
