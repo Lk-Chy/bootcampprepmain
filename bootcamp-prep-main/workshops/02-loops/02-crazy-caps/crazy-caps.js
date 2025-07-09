87479aacae495cbeecedfb9e4760482e4b94e3a3
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-09 11:58:22 +0545

    copleted crazycaps

--- bootcamp-prep-main/workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ bootcamp-prep-main/workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+
+// ### Crazy Caps
+
+// Define a function, `crazyCaps`, that accepts a string as an argument.
+// `crazyCaps` should return a string in which every other character is
+// capitalized. The first letter should be lower-cased.
+
+// ```javascript
+crazyCaps('fullstack is amazing'); // => fUlLsTaCk iS AmAzInG!
+// ```
+
+
+function crazyCaps(a){
+   let z='';
+for(let i=0;i< a.length; i++){
+    if (i%2 === 0){
+        z=z+a[i].toLowerCase();
+    }
+    else{
+        z=z+a[i].toUpperCase();
+    }
+}
+    return z;
+}
