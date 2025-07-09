02defc2702ee9d0046dc9e62688aa303c9a75f2c
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-09 17:47:26 +0545

    completed exponiate

--- bootcamp-prep-main/workshops/02-loops/04-exponentiate/exponentiate.js
+++ bootcamp-prep-main/workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,33 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// ### Exponentiate
+
+// Write a function `exponentiate` that accepts two arguments:
+// 1. base (number)
+// 2. power (number)
+
+// `exponentiate` should return the result of raising the base by the power.
+// Assume the power argument will always be an integer greater than or equal to zero.
+// Don't forget that any number raised to the 0th power is equal to 1!
+
+// Do not use the built-in [Math.pow() method](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/pow), but feel free to use it moving forward!
+
+// ```javascript
+exponentiate(2, 2)    // => 4
+exponentiate(3, 4)    // => 81
+// ```
+
+function exponentiate(base , power){
+    debugger
+    let a=1;
+    if (power <= 0){
+        if (power===0){
+            a= 1;
+        }
+        }
+        else{
+            for (let i=0;i<power;i++){
+                a=a*base
+            }
+        }
+return a;
+}
