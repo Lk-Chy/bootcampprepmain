07eebd506d760f6820cfd0fb1abed3b88631994a
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-11 20:11:09 +0545

    completed reverse

--- bootcamp-prep-main/workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ bootcamp-prep-main/workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function reverseArray(arr){
+    let  temp =[];
+    n=arr.length;
+    for (let i = 0; i< arr. length; i++){
+        temp.push(arr[i]);
+    }
+    for(let i=0; i<=n-1; i++){
+        arr[i]=temp[n-i-1];
+    }
+    return arr;
+}
