8d17936352198d086ccc16f76cfd6ea2ecd0e51d
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-11 07:48:11 +0545

    completed say my name

--- bootcamp-prep-main/workshops/08-object-methods/01-say-my-name/say-my-name.js
+++ bootcamp-prep-main/workshops/08-object-methods/01-say-my-name/say-my-name.js
@@ -1,1 +1,19 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// ### Say My Name
+
+// Define an object, `me`, that has a `name` property and a `getGreeting` method.
+// `getGreeting` should return a greeting.
+
+// ```javascript
+// console.log(me.name); // 'Kat'
+// me.getGreeting(); // => 'Hi, my name is Kat.'
+// ```
+
+
+let me = {
+  name: '',
+  getGreeting() {
+    return 'Hi, my name is ' + this.name + '.';
+  }
+};
+
