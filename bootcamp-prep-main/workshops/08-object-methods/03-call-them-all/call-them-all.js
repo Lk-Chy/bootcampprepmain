78e7cad9b39e5234b93115404e824819c3ef00bb
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-11 07:48:47 +0545

    completed call them all

--- bootcamp-prep-main/workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ bootcamp-prep-main/workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,41 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// ### Call Them All
+
+// Write a function `callThemAll`, that, given an object and a value, calls every
+// method in the object, passing in the value as the argument with each call.
+
+// `callThemAll` should return an array with all of the returned values from each
+// method invocation. The order of the elements in the returned array does not
+// matter.
+
+// ```javascript
+// let addsNums = {
+//   addTen: function(num) {
+//     return num + 10;
+//   },
+
+//   addTwenty: function(num) {
+//     return num + 20;
+//   },
+
+//   someProperty: 'value'
+// };
+
+// callThemAll(addNums, 100); // => [110, 120]
+// ```
+
+
+function callThemAll(obj, value) {
+  let result = [];
+
+  for (let key in obj) {
+    if (typeof obj[key] === 'function') {
+      let output = obj[key](value);  // call the function with the value
+      result.push(output);           // collect the return value
+    }
+  }
+  return result;
+}
+
+
+
