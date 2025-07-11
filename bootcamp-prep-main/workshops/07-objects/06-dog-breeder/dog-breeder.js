7444b329826a52652cf3e03f3732c15582365de0
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-11 07:47:46 +0545

    completed dog breeder

--- bootcamp-prep-main/workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ bootcamp-prep-main/workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,38 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// ### Dog Breeder
+
+// Define a function, `dogBreeder`, that accepts up to two optional arguments:
+//   1. name (string)
+//   2. age (number)
+
+// `dogBreeder` should return an object that represents a new dog! If the user
+// doesn't define a name, assume the dog's name is Steve. If the user doesn't
+// define the dog's age, assume the dog's age is 0.
+
+// ```javascript
+// dogBreeder('Sam', 12)    // => {name: 'Sam', age: 12}
+
+// dogBreeder(15)    // => {name:'Steve', age: 15}
+// ```
+
+
+// function dogBreeder(name='Steve', age=0){
+//  let result ={};
+//  result.name = name;
+//  result.age = age;
+// return result;
+// }
+
+
+function dogBreeder(name = 'Steve', age = 0) {
+if(typeof name === 'number'){
+    age=Number(name);
+    name='Steve';
+}
+
+
+  let result = {};
+  result.name = name;
+  result.age = age;
+  return result;
+}
