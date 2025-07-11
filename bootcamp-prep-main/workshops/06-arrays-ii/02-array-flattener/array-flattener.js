e2d871ab3705a10dd5d713ddd1695ae90027ba73
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-11 07:45:01 +0545

    completed arrray flattner

--- bootcamp-prep-main/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+// ### Array Flattener
+
+// Define a function, `arrayFlattener`, that accepts a two-dimensional array as an
+// argument.
+
+// `arrayFlattener` should return a new, one-dimensional array.
+
+// ```javascript
+arrayFlattener([1,[2, 3], 4]); // => [1, 2, 3, 4]
+// ```
+
+function arrayFlattener(arr){
+    debugger
+    let arr1=[];
+    for(i=0; i< arr.length; i++){
+        if(Array.isArray(arr[i])){
+            for(let j =0; j<arr[i].length; j++){
+                arr1[i+j]=arr[i][j];
+            }
+        }
+        else{
+            arr1[i]=arr[i];
+        }
+    }
+    return arr1;
+}
