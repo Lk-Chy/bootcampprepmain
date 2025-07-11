7beb648becfc1bc7b46f844c6b5b8e57c6ab5cf0
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-11 07:45:29 +0545

    completed make grid

--- bootcamp-prep-main/workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ bootcamp-prep-main/workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,39 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// ### Make Grid
+
+// Write a function `makeGrid` that accepts two arguments:
+// 1. numColumns (number)
+// 2. numRows (number)
+
+// `makeGrid` should return a two-dimensional array that represents a grid of the
+// given dimensions.
+
+// ```javascript
+// makeGrid(3,4);
+/* => [[1, 2, 3],
+       [1, 2, 3],
+       [1, 2, 3],
+       [1, 2, 3]]
+*/
+// ```
+
+makeGrid(3,4);
+/* => [[1, 2, 3],
+       [1, 2, 3],
+       [1, 2, 3],
+       [1, 2, 3]]
+*/
+
+function makeGrid(numColumns, numRows){
+    debugger 
+    let arr=[];
+for (let i=0; i<numRows; i++){
+    let row =[];
+    for(let k=1; k<=numColumns; k++){
+        row.push(k);
+    }
+    arr.push(row);
+}
+return arr;
+}
+
