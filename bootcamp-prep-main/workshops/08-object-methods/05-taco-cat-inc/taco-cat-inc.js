0afd7bee6af990b5373cfa630d796183d6e81298
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-13 11:45:14 +0545

    completed taco inc

--- bootcamp-prep-main/workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
+++ bootcamp-prep-main/workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
@@ -23,3 +23,57 @@ let tacoCatInc = {
 };
 
 // YOUR CODE BELOW
+// tacoCatInc.currentInventory = function(){
+//   debugger
+// let sum = 0;
+// let keys = Object.keys(tacoCatInc); // [gourmetShell, gourmetFishFilling, gourmetVeggie, gourmetSeasoning] 
+// for(let i = 0; i<keys.length; i++){
+//   let key = keys[i];  //gourmetShell --> it is an object
+//   let ke = Object.keys(key); //[hard treat shell ,soft treat shell]
+//   for(let expn in ke){
+//     let abc = 0;
+//     abc = ke.cost * ke.quantity;
+//     sum=sum+abc;
+//   }
+//   // sum = abc+sum;
+// }
+//   return sum;
+// }
+
+
+tacoCatInc.currentInventory = function() {
+  let sum = 0;
+  let keys = Object.keys(tacoCatInc);
+  
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i]; // [gourmetShell, gourmetFishFilling, gourmetVeggie, gourmetSeasoning] 
+    
+    // skip non-object keys like 'cash' or 'currentInventory'
+    if (typeof tacoCatInc[key] !== 'object' || tacoCatInc[key] === null) continue;
+    
+    let nestedKeys = Object.keys(tacoCatInc[key]);
+    
+    for (let j = 0; j < nestedKeys.length; j++) {
+      let itemKey = nestedKeys[j];
+      let item = tacoCatInc[key][itemKey];
+      
+      sum += item.cost * item.quantity;
+    }
+  }
+  
+  return sum;
+};
+
+tacoCatInc.sale = function (order) {
+  let totalPrice = 0;
+  let orderitem = Object.keys(order);
+  for (let i = 0; i < orderitem.length; i++){
+    let cate = orderitem[i];
+    let itm_name = order[cate];
+    let item = tacoCatInc[cate][itm_name];
+    totalPrice += item.cost;
+    item.quantity -= 1
+  }
+  tacoCatInc.cash += totalPrice;
+  return totalPrice;
+}
