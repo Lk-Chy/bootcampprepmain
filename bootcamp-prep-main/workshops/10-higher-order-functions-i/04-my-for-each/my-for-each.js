c1bff7e010a888fc6b0103e541df73fb6eb1d5af
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-13 11:46:42 +0545

    completed my for each

--- bootcamp-prep-main/workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ bootcamp-prep-main/workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,1 +1,27 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// ### My For Each
+
+// Write a function myForEach that accepts an array and a callback function. The
+// behavior of myForEach should mirror the functionality of the native .forEach()
+// array method as closely as possible
+
+// ```javascript
+// let sum = 0;
+
+// function addToSum(num) {
+//     sum += num;
+// }
+
+// let nums = [1, 2, 3];
+
+// myForEach(nums, addToSum);
+
+// console.log(sum); // 6
+// ```
+
+
+function myForEach(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i, array);
+  }
+}
