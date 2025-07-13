2908e3f2b55e9d055b78bb67d15257a61ccb19b7
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-13 11:46:23 +0545

    copleted whoes a special

--- bootcamp-prep-main/workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ bootcamp-prep-main/workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,47 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// ### Who's a Special?
+
+// Write a function whosASpecial that takes an array of pets and returns a string
+// confirming what you already knew: all of the pets are very special!
+// Feel free to add your own special pets to the array!
+
+// Use .forEach in your answer.
+
+
+// ```javascript
+let specialPets = [{
+    name: 'Sadie',
+    species: 'cat'
+  }, {
+    name: 'Layla',
+    species: 'cat'
+  }, {
+    name: 'Bogie',
+    species: 'dog'
+  }
+];
+
+whosASpecial(specialPets);
+// // => Sadie the cat is very special! Layla the cat is very special! Bogie the dog is very special!
+// ```
+// 
+
+function whosASpecial(pets) {
+  let result = "";
+
+  pets.forEach(function(pet) {
+    result = result + " " + pet.name + ' the ' + pet.species + ' is very special!';
+  });
+
+  return result.trim(); // remove the first extra space
+}
+
+// function whosASpecial(pets) {
+//   let result = "";
+
+//   pets.forEach(function(pet) {
+//     result =result +" " pet.name+ ' the ' + pet.species+ ' is very special!';
+//   });
+
+//   return result.trim(); // remove the last space if needed
+// }
